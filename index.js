2a84d7ef47dfd40312831fc986cd0ef83201e2f3
Amaechi Chisom <amaechilegend@gmail.com>
2020-11-30 16:56:58 +0100

    minor changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const tabletColor = document.querySelectorAll('.tablet__color');
+const header =document.querySelector('.header');
 
 var colors = () =>{return '#' + Math.floor(Math.random()*16777215).toString(16);}
 
