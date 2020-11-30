ea2e21136f6ba4997004a66a11833f17356dbccc
Amaechi Chisom <amaechilegend@gmail.com>
2020-11-30 13:03:23 +0100

    first push

---
+++ index.js
@@ -0,0 +1,11 @@
+const tabletColor = document.querySelectorAll('.tablet__color');
+
+var colors = () =>{return '#' + Math.floor(Math.random()*16777215).toString(16);}
+
+tabletColor.forEach(tablet =>{
+    console.log(colors());
+    if(colors() === "#ffffff"){
+        return tablet.style.backgroundColor = "#rgb(129, 57, 21)";
+    }
+    tablet.style.backgroundColor = colors();
+})
